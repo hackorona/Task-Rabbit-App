4da57bbd9d9027beb77692ccc7e28fc73bfdfef5
R.O.S.T <oommrrii77@gmail.com>
2020-04-01 01:46:49 +0300

    finished styling deeds page

--- get-it-done/src/Pages/deeds/DeedsPage.js
+++ get-it-done/src/Pages/deeds/DeedsPage.js
@@ -1,25 +1,32 @@
 import React, { useState } from 'react';
-// import deeds from './deeds-mocks';
+import deeds from './deeds-mocks';
 import { Switch, Route, NavLink, useRouteMatch, Redirect, } from 'react-router-dom';
 import styles from './Deeds.scss';
 import ToggleButton from 'react-toggle-button';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import HelpDetails from '../../components/Help-Details';
 
-export default () => {
+const HELP_URL = 'help';
+const requests = deeds.filter(deed => deed.isRequest);
+const offers = deeds.filter(deed => !deed.isRequest);
+
+export default function () {
   const [shouldShowMap, toggleMap] = useState(true);
   const [position, setPosition] = useState([32, 34]);
 
   navigator.geolocation.getCurrentPosition(({ coords }) =>
     setPosition([coords.latitude, coords.longitude]), undefined, { enableHighAccuracy: true }
   );
+
   const { path, url } = useRouteMatch();
+  const { params } = useRouteMatch(`${path}/:route`);
 
   return (<>
     <div className="row">
       <div>
         <section className="links">
           <div>
-            <NavLink to={`${url}/help`} className="mr40">Offer to help</NavLink>
+            <NavLink to={`${url}/${HELP_URL}`} className="mr40">Offer to help</NavLink>
             <NavLink to={`${url}/be-helped`}>Request help</NavLink>
           </div>
           <div>
@@ -30,7 +37,9 @@ export default () => {
       </div>
 
       <div>
-        <button className="btn bg-primary btn__new">Create new offer</button>
+        <button className="btn bg-primary btn__new">
+          {`Create new ${params.route === HELP_URL ? 'offer' : 'request'}`}
+        </button>
         <div className="toggle">
           <span>show map?</span>
           <ToggleButton inactiveLabel="" activeLabel=""
@@ -40,12 +49,24 @@ export default () => {
       </div>
     </div>
 
-    <div className="row">
-      <div>
+    <div className="row main-content">
+      <div className="sub-route">
         <Switch>
-          <Redirect exact from={path} to={`${path}/help`}/>
-          <Route path={`${path}/help`}>hi help</Route>
-          <Route path={`${path}/be-helped`}>bes!</Route>
+          <Redirect exact from={path} to={`${path}/help`} />
+          <Route path={`${path}/help`}>
+            {
+              requests.map(deed => <HelpDetails
+                title={deed.title} description={deed.description} 
+                distance="4 km" time="42 minutes" urgency={deed.urgency} key={deed.id}/>
+              )}
+          </Route>
+          <Route path={`${path}/be-helped`}>
+          {
+              offers.map(deed => <HelpDetails
+                title={deed.title} description={deed.description} 
+                distance="4 km" time="42 minutes" key={deed.id}/>
+              )}
+          </Route>
           <Route path={`${path}/tasks`}>tasks!</Route>
         </Switch>
       </div>
---
+++ get-it-done/src/components/Help-Details.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './Help-Details.scss';
+
+export default function HelpDetails({ urgency, distance, time, title, description }) {
+    return (<>
+        <section className="help-details">
+            <img className="help-image"
+                src="https://res.cloudinary.com/thelegend27/image/upload/v1585333256/img/hammer_i4afmv.png" alt="" />
+            <div className="help-content">
+                <div>
+                    <h2 className="sub-header">{title}</h2>
+                    <p>{description}</p>
+                </div>
+                <div className="help-extra-details">
+                    {urgency && <p>Urgency: {urgency}</p>}
+                    <p>{distance} away</p>
+                    <p>{time} ago</p>
+                </div>
+            </div>
+            <div className="help-actions">
+                <button className="btn bg-secondary">Message</button>
+                <button className="btn btn-border fg-secondary">More info</button>
+            </div>
+        </section>
+        <hr className="help-divider" />
+    </>)
+}
