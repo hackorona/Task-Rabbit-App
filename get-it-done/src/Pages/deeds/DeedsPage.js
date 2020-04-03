b8a58931006c1ede7f99c91786f43514fb3df863
R.O.S.T <oommrrii77@gmail.com>
2020-04-03 18:00:23 +0300

    moved server to a seperate folder

--- get-it-done/src/Pages/deeds/DeedsPage.js
+++ get-it-done/src/Pages/deeds/DeedsPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import axios from "axios";
 //modals
 import NewOffer from "../../Layout/NewOffer";
